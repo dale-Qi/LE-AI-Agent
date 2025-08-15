6ca52461fd132f6fa3526a8d5c9196933b912129
bubblexiu <14307385+bullexiu@user.noreply.gitee.com>
2025-08-15 09:46:22 +0800

    项目初始化

---
+++ src/main/java/com/dale/leaiagent/LeAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.dale.leaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dale/leaiagent/LeAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dale.leaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
